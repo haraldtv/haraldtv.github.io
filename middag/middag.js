e8398faea8e75ac36a4ebc5a9d13e3060b2cf7bf
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-04-24 22:38:06 +0200

    Update middag.js

--- middag/middag.js
+++ middag/middag.js
@@ -4,37 +4,38 @@ let dinners = {
         "id": 1,
         "dish": "tunfiskpasta",
         "price": [1, 2],
-        "ingredients": [{
-            "tunfisk": [3, 4],
-            "fullkornpasta": [5, 6],
-            "creme fraiche": [7, 8]        
-        }]
+        "ingredients": [
+            "tunfisk",
+            "fullkornpasta",
+            "creme fraiche"     
+        ]
     },
     {
         "id": 2,
         "dish": "kjøttdeig og ris m/ egg",
         "price": [1, 2],
-        "ingredients": [{
-            "kjøttdeig": [3, 4],
-            "egg": [5, 6],
-            "ris": [7, 9]
-        }]
+        "ingredients": [
+            "kjøttdeig",
+            "egg",
+            "ris"
+        ]
     },
     {
         "id": 3,
         "dish": "Kyllinglår m/ ris og brokkoli",
         "price": [1, 2],
-        "ingredients": [{
-            "kyllinglår": [3, 4],
-            "brokkoli": [5, 6],
-            "ris": [7, 9]
-        }]
+        "ingredients": [
+            "kyllinglår",
+            "brokkoli",
+            "ris"
+        ],
     }
 ]
 }
 
 document.getElementById("visibility").style.display = "none";
 console.log("DinnerTime!");
+console.log(dinners.dishes[2].ingredients[0]);
 
 function generateDinners() {
     let a = Math.random()
@@ -59,6 +60,25 @@ function fillTable(dinnerlist) {
     document.getElementById("a5").innerHTML=dinners.dishes[dinnerlist[4]-1].dish;
     document.getElementById("a6").innerHTML=dinners.dishes[dinnerlist[5]-1].dish;
     document.getElementById("a7").innerHTML=dinners.dishes[dinnerlist[6]-1].dish;
+
+    document.getElementById("b1").innerHTML = fillIngredients(0, dinnerlist);
+    document.getElementById("b2").innerHTML = fillIngredients(1, dinnerlist);
+    document.getElementById("b3").innerHTML = fillIngredients(2, dinnerlist);
+    document.getElementById("b4").innerHTML = fillIngredients(3, dinnerlist);
+    document.getElementById("b5").innerHTML = fillIngredients(4, dinnerlist);
+    document.getElementById("b6").innerHTML = fillIngredients(5, dinnerlist);
+    document.getElementById("b7").innerHTML = fillIngredients(6, dinnerlist);
+
+    //fillIngredients(0, dinnerlist);
+}
+
+function fillIngredients(tablepos, dinnerlist) {
+    let ingredients = "";
+    for (let i = 0; i < (dinners.dishes[dinnerlist[tablepos] - 1].ingredients).length; i++) {
+        ingredients += (" " + (dinners.dishes[dinnerlist[tablepos] - 1].ingredients[i]) + "<br>");
+    }
+    console.log(ingredients);
+    return ingredients;
 }
 
 function genNum(dinnerlist) {
