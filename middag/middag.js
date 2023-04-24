e6f86aafdb8cf80e81d181500f0f70770d4db17c
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-04-24 19:40:30 +0200

    Added a button and decided to not use the json file for now

--- middag/middag.js
+++ middag/middag.js
@@ -1,14 +1,30 @@
-console.log("Test external");
-fetch("dinners.json").then(response => {
-    console.log(response.name);
-})
-
-async function generateDinner() {
-    fetch("dinners.json")
-
-    const response = await fetch(haraldtv.github.io/middag/dinners.json);
-    const dinners = await response.json;
-
-    var dinnerDisplay = doccument.getElementById("dinnerList");
-    console.log(dinners)
+let dinners = {
+    "id": 1,
+    "name": "Retter",
+    "dishes": [{
+        "dish": "tunfiskpasta",
+        "price": [1, 2],
+        "ingredients": [{
+            "tunfisk": [3, 4],
+            "fullkornpasta": [5, 6],
+            "creme fraiche": [7, 8]        
+        }]
+    },
+    {
+        "id": 2,
+        "dish": "kjøttdeig og ris m/ egg",
+        "price": [1, 2],
+        "ingredients": [{
+            "kjøttdeig": [3, 4],
+            "egg": [5, 6],
+            "ris": [7, 9]
+        }]
+    }
+]
 }
+
+
+function generateDinners() {
+    console.log(dinners.dishes[0].dish);
+}
+
