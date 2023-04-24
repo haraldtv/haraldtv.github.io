a0cc1e26007dd4f9f9839425dea21735e4aa5118
haraldtv <129341220+haraldtv@users.noreply.github.com>
2023-04-24 21:54:08 +0200

    Made the table work :D

--- middag/middag.js
+++ middag/middag.js
@@ -1,7 +1,7 @@
 let dinners = {
-    "id": 1,
     "name": "Retter",
     "dishes": [{
+        "id": 1,
         "dish": "tunfiskpasta",
         "price": [1, 2],
         "ingredients": [{
@@ -19,11 +19,22 @@ let dinners = {
             "egg": [5, 6],
             "ris": [7, 9]
         }]
+    },
+    {
+        "id": 3,
+        "dish": "Kyllinglår m/ ris og brokkoli",
+        "price": [1, 2],
+        "ingredients": [{
+            "kyllinglår": [3, 4],
+            "brokkoli": [5, 6],
+            "ris": [7, 9]
+        }]
     }
 ]
 }
 
-generateDinners();
+document.getElementById("visibility").style.display = "none";
+console.log("DinnerTime!");
 
 function generateDinners() {
     let a = Math.random()
@@ -34,4 +45,22 @@ function generateDinners() {
         }
     }
     console.log(dinnerlist)
+
+    document.getElementById("visibility").style.display = "block";
+
+    fillTable(dinnerlist);
+}
+
+function fillTable(dinnerlist) {
+    document.getElementById("a1").innerHTML=dinners.dishes[dinnerlist[0]-1].dish;
+    document.getElementById("a2").innerHTML=dinners.dishes[dinnerlist[1]-1].dish;
+    document.getElementById("a3").innerHTML=dinners.dishes[dinnerlist[2]-1].dish;
+    document.getElementById("a4").innerHTML=dinners.dishes[dinnerlist[3]-1].dish;
+    document.getElementById("a5").innerHTML=dinners.dishes[dinnerlist[4]-1].dish;
+    document.getElementById("a6").innerHTML=dinners.dishes[dinnerlist[5]-1].dish;
+    document.getElementById("a7").innerHTML=dinners.dishes[dinnerlist[6]-1].dish;
+}
+
+function genNum(dinnerlist) {
+    let a = Math.round(100 * Math.random() % 2) + 1;
 }
